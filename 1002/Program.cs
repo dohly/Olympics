26882027c6f4f9e47b79c9d1c562970d6781bc8e
Ivan Marinin <zverbars@mail.ru>
2014-01-14 00:45:46 +0600

    1002

--- 1002/Program.cs
+++ 1002/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 
 namespace _1002
 {
-
     class Word
     {
         static Dictionary<string, string> DigitToChar = new Dictionary<string, string>()
@@ -32,42 +31,100 @@ namespace _1002
         }
         public string Text { get; private set; }
         public string Digits { get; private set; }
-        public int CountInTelephone { get; set; }
+        public int Weight { get { return Text.Length; } }
         public Word(string text)
         {
             Text = text;
             Digits = WordToDigits(text);
         }
-
-        public void CalculateCount(string telephone)
+        public override string ToString()
         {
-            CountInTelephone = 0;
-            while (telephone.IndexOf(Digits)!=-1)
-            {
-                telephone = telephone.Remove(telephone.IndexOf(Digits), Digits.Length);
-                CountInTelephone++;
-            }
+            return string.Format("{0} - {1}", Text, Digits);
         }
     }
+
     class TelephonesSolution
     {
         public string Telephone;
         public List<Word> Words = new List<Word>();
-       
+
+
         public string GetResult()
         {
-            List<List<Word>> result = new List<List<Word>>();
-            Words = Words.OrderByDescending(x => x.Digits.Length).ToList();
-            foreach (var word in Words)
+            string noSolution = "No solution.";
+            Dictionary<int, List<Word>> result = new Dictionary<int, List<Word>>();
+            for (int i = 0; i < Telephone.Length; i++)
             {
-                word.CalculateCount(Telephone);
+                string tel = Telephone.Substring(i);
+                var start = Words.Where(x => tel.StartsWith(x.Digits));
+                bool any = start.Any();
+                if (i == 0)
+                {
+                    //Есть ли первое слово
+                    if (!any)
+                    {
+                        return noSolution;
+                    }
+                    //Есть ли решение в одно слово
+                    Word singleWord = start.FirstOrDefault(x => x.Digits == Telephone);
+                    if (singleWord != null)
+                    {
+                        return singleWord.Text;
+                    }
+                }
+                if (any)
+                {
+                    result.Add(i, new List<Word>(start));
+                }
             }
-            if (result.Any())
+            int maxWordCount = result.Count;
+            if (maxWordCount != 0)
             {
-                var best = result.OrderBy(x => x.Count).First();
-                return string.Join(" ", best.Select(x => x.Text));
+                var start = result[0];
+
+                int maxLength = Telephone.Length;
+                List<List<Word>> solutions=new List<List<Word>>();
+                foreach (var word in start)
+                {
+                    string tel = Telephone.Substring(word.Weight);
+                    List<Word> solution = new List<Word>();
+                    solution.Add(word);
+                    int totalLength = word.Weight;
+                    foreach (var pair in result)
+                    {
+                        if (pair.Key < totalLength)
+                        {
+                            continue;
+                        }
+                        var words = pair.Value;
+                        foreach (Word nextWord in words)
+                        {
+                            int l = totalLength + nextWord.Weight;
+                            if (l > maxLength)
+                            {
+                                continue;
+                            }
+                            totalLength = l;
+                            solution.Add(nextWord);
+                            break;
+                        }
+                        if (totalLength == maxLength)
+                        {
+                            break;
+                        }
+                    }
+                    if (totalLength == maxLength)
+                    {
+                        solutions.Add(solution);
+                    }
+                }
+                var best = solutions.OrderBy(x => x.Count).FirstOrDefault();
+                if (best != null)
+                {
+                    return string.Join(" ", best.Select(x => x.Text));
+                }
             }
-            return "No solution.";
+            return noSolution;
         }
     }
 
@@ -86,8 +143,8 @@ namespace _1002
                             {
                                 new Word("it"),
                                 new Word("your"),
-                                new Word("reality"),
                                 new Word("real"),
+                                new Word("reality"),
                                 new Word("our")
                             }
                     };
