943d0d3c1dd24f969cb2d779d733fae5ea556a15
zverbars@mail.ru <zverbars@mail.ru>
2014-01-18 14:13:18 +0600

    1009

---
+++ 1009/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int K = int.Parse(Console.ReadLine());
+            int[] a = new int[18];
+            a[0] = 0;
+            K = K - 1;
+            a[1] = K;
+            for (int j = 2; j < N+1; j++)
+            {
+                //modified fibonacci
+                a[j] = a[j - 2]*(K) + a[j - 1]*K;
+            }
+            Console.WriteLine(a[N]+a[N-1]);
+
+        }
+    }
+}
