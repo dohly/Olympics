afe75b4b7513fc2c5be4052fc5a09ae115cad864
zverbars@mail.ru <zverbars@mail.ru>
2014-01-18 22:14:01 +0600

    1293

---
+++ 1110/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace _1110
+{
+    class Program
+    {
+        static bool IsCorrect(int X, int N, int M, int Y)
+        {
+            var pow = new BigInteger(X);
+            var left = BigInteger.ModPow(pow, N, M);
+            return left == new BigInteger(Y);
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            var inputs = s.Split(new string[] { " " }, StringSplitOptions.None);
+            int N = int.Parse(inputs[0]);
+            int M = int.Parse(inputs[1]);
+            int Y = int.Parse(inputs[2]);
+            var range = Enumerable.Range(0, M - 1);
+            var result = range.Where(x => IsCorrect(x, N, M, Y)).ToList();
+            if (result.Count == 0)
+            {
+                Console.WriteLine(-1);
+            }
+            s = string.Join(" ", result);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ 1293/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1293
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var inputs = input.Split(new string[] {" "}, StringSplitOptions.None);
+            int N = int.Parse(inputs[0]);
+            int A = int.Parse(inputs[1]);
+            int B = int.Parse(inputs[2]);
+            Console.WriteLine(N*A*B*2);
+        }
+    }
+}
