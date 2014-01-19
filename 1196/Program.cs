19447f638446617d119c6c59d8ac9ff2e5130bd7
zverbars@mail.ru <zverbars@mail.ru>
2014-01-19 20:54:40 +0600

    1196

---
+++ 1196/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1196
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            HashSet<string> teacher = new HashSet<string>();
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine();
+                if (!teacher.Contains(s))
+                {
+                    teacher.Add(s);
+                }
+            }
+            int M = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = 0; i < M; i++)
+            {
+                string s = Console.ReadLine();
+                if (teacher.Contains(s))
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
