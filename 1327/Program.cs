5f681c3c3a79565cedba6f330736ef5d0e511fe2
zverbars@mail.ru <zverbars@mail.ru>
2014-01-19 23:10:12 +0600

    1327

---
+++ 1327/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1327
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = A; i < B+1; i++)
+            {
+                if (i%2 != 0)
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
