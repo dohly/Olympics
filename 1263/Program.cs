374e8b2794b99647422a3b2ada35da113ad34d31
zverbars@mail.ru <zverbars@mail.ru>
2014-01-19 12:53:48 +0600

    1263

---
+++ 1263/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1263
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
