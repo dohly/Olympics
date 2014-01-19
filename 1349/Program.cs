f59a8144345bfc56aa0f2db21f9f6c19f224ceed
zverbars@mail.ru <zverbars@mail.ru>
2014-01-19 23:29:18 +0600

    1349

---
+++ 1349/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1349
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            if (N == 1)
+            {
+                Console.WriteLine("1 2 3");
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
