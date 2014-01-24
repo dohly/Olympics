6623d30e7f3089e88734e8c13b6dc9f0018a0184
zverbars@mail.ru <zverbars@mail.ru>
2014-01-24 23:18:11 +0600

    1493

---
+++ 1493/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1493
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            var a = s.Substring(0, 3).Select(x => Char.GetNumericValue(x)).Sum();
+            var b = int.Parse(s.Substring(3));
+            b++;
+            double a2 = b.ToString().Select(x => Char.GetNumericValue(x)).Sum();
+            if (a2 == a)
+            {
+                Console.WriteLine("Yes");
+                return;
+            }
+            b = b - 2;
+            a2 = b.ToString().Select(x => Char.GetNumericValue(x)).Sum();
+            if (a2 == a)
+            {
+                Console.WriteLine("Yes");
+                return;
+            }
+            Console.WriteLine("No");
+        }
+    }
+}
