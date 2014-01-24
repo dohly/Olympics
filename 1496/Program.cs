3ab4e4b557ae7829cd94fa1d88ceef43e5940e15
zverbars@mail.ru <zverbars@mail.ru>
2014-01-24 23:26:15 +0600

    1496

---
+++ 1496/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1496
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<string> result = new List<string>();
+            List<string> accounts = new List<string>();
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine();
+                if (accounts.Any(x => x == s) && result.All(x => x != s))
+                {
+                    result.Add(s);
+                }
+                else
+                {
+                    accounts.Add(s);
+                }
+            }
+            foreach (var VARIABLE in result)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+    }
+}
