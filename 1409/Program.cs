3808928d1150368d2490ef760d9f374c0593de83
zverbars@mail.ru <zverbars@mail.ru>
2014-01-24 22:21:30 +0600

    1409

---
+++ 1409/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inp = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Console.WriteLine("{0} {1}", inp[1] - 1, inp[0] - 1);
+        }
+    }
+}
