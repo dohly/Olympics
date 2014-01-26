f20e250650d662bc47e95b52d7b03cad777570db
zverbars@mail.ru <zverbars@mail.ru>
2014-01-26 14:29:11 +0600

    1563

---
+++ 1563/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1563
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<string> magazines=new List<string>();
+            int bayan = 0;
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine();
+                if (magazines.Any(x => x == s))
+                {
+                    bayan++;
+                }
+                else
+                {
+                    magazines.Add(s);
+                }
+            }
+            Console.WriteLine(bayan);
+        }
+    }
+}
