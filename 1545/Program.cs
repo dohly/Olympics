fa5bb61adcec1cc587e9ef5a6f2ddebf1d120728
zverbars@mail.ru <zverbars@mail.ru>
2014-01-26 14:17:58 +0600

    1545 1585

---
+++ 1545/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1545
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<string> ie=new List<string>();
+            for (int i = 0; i < N; i++)
+            {
+                ie.Add(Console.ReadLine());
+            }
+            string k = Console.ReadLine();
+            ie = ie.Where(x => x.StartsWith(k)).ToList();
+            if (ie.Count != 0)
+            {
+                foreach (string s in ie)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
---
+++ 1585/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1585
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Dictionary<string, int> result = new Dictionary<string, int>()
+                {
+                    {"Emperor Penguin",0},
+                    {"Macaroni Penguin",0},
+                    {"Little Penguin",0}
+                };
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine();
+                result[s]++;
+            }
+            var r=result.OrderByDescending(x => x.Value).First();
+            Console.WriteLine(r.Key);
+        }
+    }
+}
