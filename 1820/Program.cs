9c81e5a746d3c7d39d3702eaf2795b3038c9ad5f
zverbars@mail.ru <zverbars@mail.ru>
2014-01-27 21:37:16 +0600

    1820

---
+++ 1820/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1820
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] i = Console.ReadLine().Split().Select(x => float.Parse(x)).ToArray();
+            float n = i[0];
+            float k = i[1];
+            if (k >= n)
+            {
+                Console.WriteLine(2);
+                return;
+            }
+            double result = Math.Ceiling(n/k*2);
+            Console.WriteLine(result);
+        }
+    }
+}
